40c74a12307d1d13d408344645c66d1532d97302
Joseph Garrone <joseph.garrone.gj@gmail.com>
2025-01-03 22:56:21 +0100

    Update keycloakify

--- src/login/i18n.ts
+++ src/login/i18n.ts
@@ -1,11 +1,9 @@
-import { i18nBuilder } from '@keycloakify/svelte/login/i18n/i18nBuilder';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { i18nBuilder } from '@keycloakify/svelte/login';
 import type { ThemeName } from '../kc.gen';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const { useI18n, ofTypeI18n } = i18nBuilder
-  .withThemeName<ThemeName>() // See: https://docs.keycloakify.dev/theme-variants#different-text-for-each-of-your-theme-variants
-  .withExtraLanguages({}) // See: https://docs.keycloakify.dev/i18n/adding-support-for-extra-languages
-  .withCustomTranslations({}) // See: https://docs.keycloakify.dev/i18n/adding-new-translation-messages-or-changing-the-default-ones
-  .build();
+/** @see: https://docs.keycloakify.dev/features/i18n */
+const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>().build();
+
 type I18n = typeof ofTypeI18n;
 export { useI18n, type I18n };
