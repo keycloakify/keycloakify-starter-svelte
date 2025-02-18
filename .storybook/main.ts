e9f31db5aaffd5011ba09bec3788c20128c705cf
garronej <joseph.garrone@protonmail.com>
2025-02-18 16:50:29 +0100

    Fix storybook setup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,12 +2,7 @@ import type { StorybookConfig } from '@storybook/svelte-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|ts|svelte)'],
-  addons: [
-    '@storybook/addon-svelte-csf',
-    '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
-  ],
+  addons: ['@storybook/addon-svelte-csf', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/svelte-vite',
     options: {},
