2eabee1f489f0c7707d24c6e19578593afcf5712
Luca Peruzzo <peruzzoluca98@gmail.com>
2025-08-19 08:49:31 +0200

    feat: setup base core for login v2

---
+++ src/@keycloakify/login-ui/core/darkMode.ts
@@ -0,0 +1,41 @@
+import { insertOrUpdateMetaTag, removeMetaTag } from '../../login-ui-svelte/tools/metaTag';
+
+export function initializeDarkMode(params: { htmlDarkModeClassName: string }) {
+  const { htmlDarkModeClassName } = params;
+
+  const setColorScheme = (params: { isDark: boolean }) => {
+    const { isDark } = params;
+    document.documentElement.classList[isDark ? 'add' : 'remove'](htmlDarkModeClassName);
+
+    insertOrUpdateMetaTag({
+      name: 'color-scheme',
+      content: isDark ? 'dark' : 'light',
+    });
+  };
+
+  const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+  setColorScheme({
+    isDark: mediaQuery.matches,
+  });
+
+  const onChange = (event: MediaQueryListEvent) => {
+    setColorScheme({
+      isDark: event.matches,
+    });
+  };
+
+  mediaQuery.addEventListener('change', onChange);
+
+  function cleanup() {
+    mediaQuery.removeEventListener('change', onChange);
+
+    setColorScheme({
+      isDark: false,
+    });
+
+    removeMetaTag({ name: 'color-scheme' });
+  }
+
+  return { cleanup };
+}
