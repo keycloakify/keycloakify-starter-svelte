9b3207277993e6aa48ebf2af5ca774926b646311
Luca Peruzzo <peruzzoluca98@gmail.com>
2025-08-20 16:35:12 +0200

    feat: add link idp action page

---
+++ src/login/pages/link-idp-action/Form.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { assert } from 'tsafe';
+  import { useKcClsx } from '../../../@keycloakify/login-ui-svelte/useKcClsx';
+  import ActionGroup from '../../components/buttons/ActionGroup.svelte';
+  import Button from '../../components/buttons/Button.svelte';
+  import { useI18n } from '../../i18n';
+  import { useKcContext } from '../../KcContext.gen';
+
+  const { kcContext } = useKcContext();
+  assert(kcContext.pageId === 'link-idp-action.ftl');
+
+  const { msg } = useI18n();
+  const { kcClsx } = useKcClsx();
+
+  const { url, idpDisplayName } = kcContext;
+</script>
+
+<div
+  id="kc-link-text"
+  class={kcClsx('kcContentWrapperClass')}
+>
+  {@render msg('linkIdpActionMessage', idpDisplayName)()}
+</div>
+<form
+  action={url.loginAction}
+  class={kcClsx('kcFormClass')}
+  method="post"
+>
+  <ActionGroup>
+    <Button
+      class={kcClsx('kcButtonPrimaryClass')}
+      id="kc-continue"
+      name="continue"
+      type="submit"
+    >
+      {@render msg('doContinue')()}
+    </Button>
+    <Button
+      class={kcClsx('kcButtonSecondaryClass')}
+      id="kc-cancel"
+      name="cancel-aia"
+      type="submit"
+    >
+      {@render msg('doCancel')()}
+    </Button>
+  </ActionGroup>
+</form>
---
+++ src/login/pages/link-idp-action/Page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { assert } from 'tsafe/assert';
+  import { useI18n } from '../../../@keycloakify/login-ui-svelte/i18n/useI18n';
+  import { useKcContext } from '../../KcContext.gen';
+  import { Template } from '../../components/Template';
+  import Form from './Form.svelte';
+
+  const { kcContext } = useKcContext();
+  assert(kcContext.pageId === 'link-idp-action.ftl');
+
+  const { msg } = useI18n();
+</script>
+
+{#snippet form()}
+  <Form />
+{/snippet}
+
+<Template
+  displayMessage={false}
+  slots={{
+    header: msg('linkIdpActionTitle', kcContext.idpDisplayName),
+    form: form,
+  }}
+/>
