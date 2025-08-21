060fc4036e35bbfb7df0f2d0a22342852fad8fb5
Luca Peruzzo <peruzzoluca98@gmail.com>
2025-08-21 10:10:20 +0200

    fix: add button props to login button

--- src/login/components/buttons/LoginButton.svelte
+++ src/login/components/buttons/LoginButton.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
+  import type { HTMLButtonAttributes } from 'svelte/elements';
   import { useReducer } from '../../../@keycloakify/login-ui-svelte/tools/useReducer';
   import { useKcClsx } from '../../../@keycloakify/login-ui-svelte/useKcClsx';
   import { useI18n } from '../../i18n';
   import ActionGroup from './ActionGroup.svelte';
   import Button from './Button.svelte';
 
-  type Props = {
+  type Props = HTMLButtonAttributes & {
     className?: string;
   };
-  const { className }: Props = $props();
+  const { className, ...props }: Props = $props();
   const { kcClsx } = useKcClsx();
   const { msg } = useI18n();
   const [hasBeenClicked, setClicked] = useReducer(() => true, false);
@@ -16,6 +17,7 @@
 
 <ActionGroup {className}>
   <Button
+    {...props}
     class={kcClsx('kcButtonPrimaryClass', 'kcButtonBlockClass')}
     id="kc-login"
     name="login"
