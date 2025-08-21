15381bdd7ae0351accae753a8cb79ccf7b77701e
Luca Peruzzo <peruzzoluca98@gmail.com>
2025-08-21 11:25:06 +0200

    fix: set i18n typings

--- src/@keycloakify/login-ui-svelte/i18n/i18nContext.ts
+++ src/@keycloakify/login-ui-svelte/i18n/i18nContext.ts
@@ -3,10 +3,10 @@ import type { createUseI18n } from './getI18n';
 
 export const I18N_KEY = Symbol('kcI18n');
 
-export const setI18nContext = (i18n: ReturnType<typeof createUseI18n>['ofTypeI18n']) => {
-  setContext<ReturnType<typeof createUseI18n>['ofTypeI18n'] | undefined>(I18N_KEY, i18n);
+export const setI18nContext = <I18n extends ReturnType<typeof createUseI18n>['ofTypeI18n']>(i18n: I18n) => {
+  setContext<I18n | undefined>(I18N_KEY, i18n);
 };
 
-export const getI18nContext = () => {
-  return getContext<ReturnType<typeof createUseI18n>['ofTypeI18n'] | undefined>(I18N_KEY);
+export const getI18nContext = <I18n extends ReturnType<typeof createUseI18n>['ofTypeI18n']>() => {
+  return getContext<I18n | undefined>(I18N_KEY);
 };
--- src/@keycloakify/login-ui-svelte/i18n/useI18n.ts
+++ src/@keycloakify/login-ui-svelte/i18n/useI18n.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { createUseI18n } from './getI18n';
 import { getI18nContext } from './i18nContext';
 
-export function useI18n(): ReturnType<typeof createUseI18n>['ofTypeI18n'] {
-  const contextValue = getI18nContext();
+export function useI18n<I18n extends ReturnType<typeof createUseI18n<any, any>>['ofTypeI18n']>(): I18n {
+  const contextValue = getI18nContext<I18n>();
 
   if (contextValue === undefined) {
     throw new Error('useI18n must be used within a I18nProvider');
