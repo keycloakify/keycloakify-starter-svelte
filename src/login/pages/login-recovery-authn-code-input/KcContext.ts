160917886865acd9d56748b422790407409aa85d
Luca Peruzzo <peruzzoluca98@gmail.com>
2025-08-21 13:02:34 +0200

    feat: add login recovery authn code input page

--- src/login/pages/login-recovery-authn-code-input/KcContext.ts
+++ src/login/pages/login-recovery-authn-code-input/KcContext.ts
@@ -2,9 +2,15 @@ import { type KcContextCommon, kcContextCommonMock } from '../../components/Temp
 
 export type KcContext = KcContextCommon & {
   pageId: 'login-recovery-authn-code-input.ftl';
+  recoveryAuthnCodesInputBean: {
+    codeNumber: number;
+  };
 };
 
 export const kcContextMock: KcContext = {
   ...kcContextCommonMock,
   pageId: 'login-recovery-authn-code-input.ftl',
+  recoveryAuthnCodesInputBean: {
+    codeNumber: 1234,
+  },
 };
