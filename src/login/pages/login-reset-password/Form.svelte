2175a8bccce5f852f523ea890ed1a0e1230ad3c9
Luca Peruzzo <peruzzoluca98@gmail.com>
2025-08-21 14:18:57 +0200

    feat: add login reset password page

--- src/login/pages/login-reset-password/Form.svelte
+++ src/login/pages/login-reset-password/Form.svelte
@@ -1,2 +1,52 @@
 <script lang="ts">
+  import { assert } from 'tsafe';
+  import { kcSanitize } from '../../../@keycloakify/login-ui-svelte/kcSanitize';
+  import { useKcClsx } from '../../../@keycloakify/login-ui-svelte/useKcClsx';
+  import LoginButton from '../../components/buttons/LoginButton.svelte';
+  import Input from '../../components/field/Input.svelte';
+  import { useI18n } from '../../i18n';
+  import { useKcContext } from '../../KcContext.gen';
+
+  const { kcContext } = useKcContext();
+  assert(kcContext.pageId === 'login-reset-password.ftl');
+  const { msg } = useI18n();
+  const { kcClsx } = useKcClsx();
+
+  const { url, auth, messagesPerField, realm } = kcContext;
 </script>
+
+<form
+  id="kc-recovery-code-login-form"
+  class={kcClsx('kcFormClass')}
+  action={url.loginAction}
+  method="post"
+>
+  {#snippet input(inputProps: { type: 'text'; id: string; 'aria-invalid': 'true' | undefined })}
+    <input
+      type={inputProps.type}
+      aria-invalid={inputProps['aria-invalid']}
+      value={auth?.attemptedUsername}
+      id="username"
+      name="username"
+      autofocus
+    />
+  {/snippet}
+  {#snippet label()}
+    {#if !realm.loginWithEmailAllowed}
+      {@render msg('username')()}
+    {:else if !realm.registrationEmailAsUsername}
+      {@render msg('usernameOrEmail')()}
+    {:else}
+      {@render msg('email')()}
+    {/if}
+  {/snippet}
+  {#snippet error()}
+    {kcSanitize(messagesPerField.get('username'))}
+  {/snippet}
+  <Input
+    renderInput={() => input}
+    {label}
+    error={messagesPerField.existsError('username') ? error : undefined}
+  />
+  <LoginButton type="submit"></LoginButton>
+</form>
--- src/login/pages/login-reset-password/KcContext.ts
+++ src/login/pages/login-reset-password/KcContext.ts
@@ -2,9 +2,32 @@ import { type KcContextCommon, kcContextCommonMock } from '../../components/Temp
 
 export type KcContext = KcContextCommon & {
   pageId: 'login-reset-password.ftl';
+  realm: {
+    loginWithEmailAllowed: boolean;
+    duplicateEmailsAllowed: boolean;
+  };
+  url: {
+    loginResetCredentialsUrl: string;
+  };
+  auth: {
+    attemptedUsername?: string;
+  };
 };
 
 export const kcContextMock: KcContext = {
   ...kcContextCommonMock,
   pageId: 'login-reset-password.ftl',
+  realm: {
+    ...kcContextCommonMock.realm,
+    loginWithEmailAllowed: false,
+    duplicateEmailsAllowed: false,
+  } satisfies KcContext['realm'],
+  auth: {},
+  url: {
+    loginAction: '#',
+    loginResetCredentialsUrl: '#',
+    loginRestartFlowUrl: '#',
+    loginUrl: '#',
+    ssoLoginInOtherTabsUrl: '#',
+  },
 };
