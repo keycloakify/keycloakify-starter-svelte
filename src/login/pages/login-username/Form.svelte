5382332b1b502362ce9e4004110b929f03050ee0
Luca Peruzzo <peruzzoluca98@gmail.com>
2025-08-22 16:59:16 +0200

    feat: add page login username

--- src/login/pages/login-username/Form.svelte
+++ src/login/pages/login-username/Form.svelte
@@ -1,2 +1,83 @@
 <script lang="ts">
+  import { assert } from 'tsafe';
+  import { kcSanitize } from '../../../@keycloakify/login-ui-svelte/kcSanitize';
+  import { useKcClsx } from '../../../@keycloakify/login-ui-svelte/useKcClsx';
+  import { useKcContext } from '../../KcContext.gen';
+  import ConditionalUiData from '../../components/ConditionalUIData/ConditionalUIData.svelte';
+  import LoginButton from '../../components/buttons/LoginButton.svelte';
+  import Checkbox from '../../components/field/Checkbox.svelte';
+  import Input from '../../components/field/Input.svelte';
+  import { useI18n } from '../../i18n';
+
+  const { kcContext } = useKcContext();
+  assert(kcContext.pageId === 'login-username.ftl');
+  const { msg } = useI18n();
+  const { kcClsx } = useKcClsx();
+
+  const { url, realm, login, usernameHidden, enableWebAuthnConditionalUI, messagesPerField } = kcContext;
 </script>
+
+<div id="kc-form">
+  <div id="kc-form-wrapper">
+    {#if realm.password}
+      <form
+        id="kc-form-login"
+        class={kcClsx('kcFormClass')}
+        action={url.loginAction}
+        method="post"
+      >
+        {#if !usernameHidden}
+          <div class={kcClsx('kcFormGroupClass')}>
+            {#snippet input(inputProps: { type: 'text'; id: string; 'aria-invalid': 'true' | undefined })}
+              <input
+                type={inputProps.type}
+                aria-invalid={inputProps['aria-invalid']}
+                id={inputProps.id}
+                autofocus
+                autocomplete={enableWebAuthnConditionalUI ? 'username webauthn' : 'username'}
+                value={login.username ?? ''}
+              />
+            {/snippet}
+            {#snippet label()}
+              {#if !realm.loginWithEmailAllowed}
+                {@render msg('username')()}
+              {:else if !realm.registrationEmailAsUsername}
+                {@render msg('usernameOrEmail')()}
+              {:else}
+                {@render msg('email')()}
+              {/if}
+            {/snippet}
+            {#snippet error()}
+              {kcSanitize(messagesPerField.get('username'))}
+            {/snippet}
+            <Input
+              renderInput={input}
+              {label}
+              error={messagesPerField.existsError('username') ? error : undefined}
+            />
+          </div>
+        {/if}
+
+        <div class={kcClsx('kcFormGroupClass')}>
+          {#if realm.rememberMe && !usernameHidden}
+            {#snippet checkbox(inputProps: { className: string; type: 'checkbox'; id: string })}
+              <input
+                type={inputProps.type}
+                id={inputProps.id}
+                name="rememberMe"
+                value={login.rememberMe}
+              />
+            {/snippet}
+            <Checkbox
+              label={msg('rememberMe')}
+              renderInput={checkbox}
+            ></Checkbox>
+          {/if}
+        </div>
+
+        <LoginButton type="submit"></LoginButton>
+      </form>
+    {/if}
+  </div>
+</div>
+<ConditionalUiData></ConditionalUiData>
--- src/login/pages/login-username/Page.svelte
+++ src/login/pages/login-username/Page.svelte
@@ -6,18 +6,28 @@
   import Form from './Form.svelte';
 
   const { kcContext } = useKcContext();
-  assert(kcContext.pageId === 'login.ftl');
+  assert(kcContext.pageId === 'login-username.ftl');
 
   const { msg } = useI18n();
+  const { realm, social, registrationDisabled, messagesPerField, url } = kcContext;
 </script>
 
 {#snippet form()}
   <Form />
 {/snippet}
 
+{#snippet info()}
+  <div id="kc-registration">
+    <span>{@render msg('noAccount')()} <a href={url.registrationUrl}>{@render msg('doRegister')()}</a></span>
+  </div>
+{/snippet}
+
 <Template
+  displayMessage={!messagesPerField.existsError('username')}
+  displaySocialProviders={!!realm.password && !!social?.providers && !!social.providers.length}
   slots={{
-    header: msg('doLogIn'),
+    header: msg('loginAccountTitle'),
     form: form,
+    info: realm.password && realm.registrationAllowed && !registrationDisabled ? info : undefined,
   }}
 />
