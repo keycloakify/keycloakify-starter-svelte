f840fad69ce8ae4577d864c63dde5edaa31e2f9d
Luca Peruzzo <peruzzoluca98@gmail.com>
2025-08-22 16:59:16 +0200

    feat: add page login username

--- src/login/pages/login-username/KcContext.ts
+++ src/login/pages/login-username/KcContext.ts
@@ -2,9 +2,58 @@ import { type KcContextCommon, kcContextCommonMock } from '../../components/Temp
 
 export type KcContext = KcContextCommon & {
   pageId: 'login-username.ftl';
+  url: {
+    loginResetCredentialsUrl: string;
+    registrationUrl: string;
+  };
+  realm: {
+    loginWithEmailAllowed: boolean;
+    rememberMe: boolean;
+    password: boolean;
+    resetPasswordAllowed: boolean;
+    registrationAllowed: boolean;
+  };
+  registrationDisabled: boolean;
+  login: {
+    username?: string;
+    rememberMe?: string;
+  };
+  usernameHidden?: boolean;
+  social?: {
+    displayInfo: boolean;
+    providers?: {
+      loginUrl: string;
+      alias: string;
+      providerId: string;
+      displayName: string;
+      iconClasses?: string;
+    }[];
+  };
 };
 
 export const kcContextMock: KcContext = {
   ...kcContextCommonMock,
   pageId: 'login-username.ftl',
+  url: {
+    loginAction: '#',
+    loginResetCredentialsUrl: '#',
+    loginRestartFlowUrl: '#',
+    loginUrl: '#',
+    ssoLoginInOtherTabsUrl: '#',
+    registrationUrl: '#',
+  },
+  realm: {
+    ...kcContextCommonMock.realm,
+    loginWithEmailAllowed: true,
+    rememberMe: true,
+    password: true,
+    resetPasswordAllowed: true,
+    registrationAllowed: true,
+  },
+  social: {
+    displayInfo: true,
+  },
+  usernameHidden: false,
+  login: {},
+  registrationDisabled: false,
 };
