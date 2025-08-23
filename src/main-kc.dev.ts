10d38bbf2067bd79921e51ae30c63c2618b6cbdf
Luca Peruzzo <peruzzoluca98@gmail.com>
2025-08-23 11:06:23 +0200

    feat: add page webauthn register

--- src/main-kc.dev.ts
+++ src/main-kc.dev.ts
@@ -3,7 +3,7 @@ import KcPage from './kc.gen.svelte';
 import { getKcContextMock } from './login/mocks/getKcContextMock.gen';
 
 const kcContext = getKcContextMock({
-  pageId: 'webauthn-error.ftl',
+  pageId: 'login.ftl',
   overrides: {},
 });
 
